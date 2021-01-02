541a2724f9777d578582241daad5dd12365f1aa9
Akshay Shil <akshayshil34@gmail.com>
2021-01-02 22:52:53 +0530

    Done work
    fixed table
    added bootstrap
    fixed image

--- worklist/src/app/request/request.component.ts
+++ worklist/src/app/request/request.component.ts
@@ -9,6 +9,11 @@ export class RequestComponent implements OnInit {
 
   constructor() { }
 
+  headers = ['col1','col2','col3','col4','col5',"col6"];
+  results = [
+    ["Sam","123","Types","Hello","World","Ninja"],
+    ["Sam","123","Types","Hello","World","Ninja"]
+  ];
   ngOnInit(): void {
   }
 
